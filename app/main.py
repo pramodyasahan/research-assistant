523f152b95ea02495f53d4340e2a84c169f449dc
pramodyasahan <pramodyasahan.edu@gmail.com>
2025-03-30 21:56:04 +0530

    [add] endpoint validation to accept only educational questions

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI, HTTPException, Body
-from app.models.schema import QueryInput
+from fastapi import FastAPI, HTTPException, status
+from models.schema import QueryInput
+from utils.filters import is_valid_question
 
 app = FastAPI()
 
 
 @app.post('/question')
 async def ask_question(payload: QueryInput):
-    if not payload:
-        raise HTTPException("Need to ask a question")
+    if not is_valid_question(payload.question):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Only educational research query are accepted")
 
-    return payload
+    return payload.question
